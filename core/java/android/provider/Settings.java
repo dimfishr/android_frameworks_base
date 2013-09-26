d0b83df58fe5f7c5177b0c3ab54f1e68d0147509
htchoi1995 <htchoi1995@gmail.com>
2013-09-26 13:01:58 +0200

    [1/2] SystemUI: Network speed indicator
    Change-Id: I88e7cdfe20342c695a33383db36bf80bb4ffe009
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    [1/2] SystemUI: Update network traffic indicator
    
    Android measures the amount of data transferred in bytes,
    however, by convention, data rates should be measured in bits
    per second.
    
    *Convert the change in total data and time to bits and seconds,
    and then divide the amount transferred by the number of
    seconds that have elapsed.
    
    *Update the labels and have them use the correct SI prefixes.
    
    *Factor out or add @hide to public methods and constructors
    
    *Remove all Runnables and Messages from Handler queue when
    disabled
    
    *Minor formatting changes to stay consistent with Android's
    code-style guidelines.
    https://source.android.com/source/code-style.html
    
    PS2: Set maximum digits to prevent it from filling the statusbar
    PS3: Rebase
    PS4: Add ability to display either bits or bytes
    
    Change-Id: I9add610a8d14f5a48f2af0a82ced0c186224e282
    
    Conflicts:
    	core/java/android/provider/Settings.java
    
    [1/2] Base: Upgrade network traffic meter
    
    *Updated class name to be less ambiguous
    *Replaced the multi-check in settings with a proper listview
    *Added the ability to view up-stream, down-stream or both
    *Added the option to change how often it refreshes
    *Added a check before it updates the status bar to prevent it from
    refreshing everything needlessly
    *Added icons to show data direction
    *Remove unused style attributes
    
    Icon artwork is courtesy android design downloads
    Some features were inspired by previous work done by 0xD34D
    
    OmniGears: https://gerrit.omnirom.org/#/c/5130/
    
    Change-Id: I9f86292a6adbc5e30967df76fae510be248fc38b
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	packages/SystemUI/res/values/custom_dimens.xml
    	packages/SystemUI/res/values/custom_styles.xml
    	packages/SystemUI/res/values/dimens.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarTransitions.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2573,6 +2573,16 @@ public final class Settings {
          */
         public static final String BATTERY_LIGHT_FULL_COLOR = "battery_light_full_color";
 
+        /**
+         * Network traffic indicator, goes from least to greatest significant bitwise
+         * 0 = Display up-stream traffic if set
+         * 1 = Display down-stream traffic if set
+         * 2 = Show as Byte/s if set
+         * 16-31 = Refresh interval(ms)  min: 250  max: 32750  default: 1000
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
+
         /** Sprint MWI Quirk: Show message wait indicator notifications
          * @hide
          */
